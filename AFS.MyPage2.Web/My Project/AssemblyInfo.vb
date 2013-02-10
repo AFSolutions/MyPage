4d2543b03d12c2d09fc54995f957d050e96ceddd
AFSolutions <af.allgemein@gmail.com>
2013-02-10 09:48:23 +0100

    v1

---
+++ AFS.MyPage2.Web/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die
+' Assemblyinformationen zu ändern.
+
+' Werte der Assemblyattribute überprüfen
+<Assembly: AssemblyTitle("AFS.MyPage2.Web")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("AFS.MyPage2.Web")> 
+<Assembly: AssemblyCopyright("Copyright ©  2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM bereitgestellt wird.
+<Assembly: Guid("3e9c25c2-fdfe-4de9-a5b6-36cb12eaca97")> 
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" wie folgt verwenden:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ AFS.MyPage2/App.xaml.vb
@@ -0,0 +1,44 @@
+﻿ Partial Public Class App
+    Inherits Application
+
+    public Sub New()
+        InitializeComponent()
+    End Sub
+    
+    Private Sub Application_Startup(ByVal o As Object, ByVal e As StartupEventArgs) Handles Me.Startup
+        Me.RootVisual = New MainPage()
+    End Sub
+    
+    Private Sub Application_Exit(ByVal o As Object, ByVal e As EventArgs) Handles Me.Exit
+
+    End Sub
+    
+    Private Sub Application_UnhandledException(ByVal sender As object, ByVal e As ApplicationUnhandledExceptionEventArgs) Handles Me.UnhandledException
+
+        ' Wenn die Anwendung außerhalb des Debuggers ausgeführt wird, melden Sie die Ausnahme mithilfe
+        ' des Ausnahmemechanismus des Browsers. In IE wird hier ein gelbes Warnsymbol in der 
+        ' Statusleiste angezeigt, Firefox zeigt einen Skriptfehler an.
+        If Not System.Diagnostics.Debugger.IsAttached Then
+
+            ' Hinweis: So kann die Anwendung weiterhin ausgeführt werden, nachdem eine Ausnahme ausgelöst, aber nicht
+            ' behandelt wurde. 
+            ' Bei Produktionsanwendungen sollte diese Fehlerbehandlung durch eine Anwendung ersetzt werden, die 
+            ' den Fehler der Website meldet und die Anwendung beendet.
+            e.Handled = True
+            Deployment.Current.Dispatcher.BeginInvoke(New Action(Of ApplicationUnhandledExceptionEventArgs)(AddressOf ReportErrorToDOM), e)
+        End If
+    End Sub
+
+   Private Sub ReportErrorToDOM(ByVal e As ApplicationUnhandledExceptionEventArgs)
+
+        Try
+            Dim errorMsg As String = e.ExceptionObject.Message + e.ExceptionObject.StackTrace
+            errorMsg = errorMsg.Replace(""""c, "'"c).Replace(ChrW(13) & ChrW(10), "\n")
+
+            System.Windows.Browser.HtmlPage.Window.Eval("throw new Error(""Unhandled Error in Silverlight Application " + errorMsg + """);")
+        Catch
+        
+        End Try
+    End Sub
+    
+End Class
